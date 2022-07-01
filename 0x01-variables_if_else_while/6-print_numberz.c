9b846a714ca32c93558b07e078bcb819d49a5a58
Brian Kimathi Muriuki <briankimathi>
2022-07-01 23:38:34 +0000

    char base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - end func
+ *
+ * Return: End prog
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+		putchar(n);
+
+	putchar(10);
+
+	return (0);
+}
