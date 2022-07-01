92d1dfdafee618260f1ea17cc9ff5dbb08b174cd
Brian Kimathi Muriuki <briankimathi>
2022-07-01 23:52:22 +0000

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-main func
+ *
+ * Return: end prog
+ */
+int main(void)
+{
+	int a = '0';
+	int b = 'a';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
