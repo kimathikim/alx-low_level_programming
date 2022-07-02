c2e108d1532e21cd17311836e2278a4792b28507
Brian Kimathi Muriuki <briankimathi>
2022-07-02 00:42:22 +0000

    possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - main func
+ *
+ * Return: end prog
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int coma = 1;
+
+	while (a <= 9)
+	{
+		while (b <= 9)
+		{
+			if (a < b)
+			{
+				if (coma == 0)
+				{
+				putchar(',');
+				putchar(32);
+			}
+			coma = 0;
+			putchar(a + '0');
+			putchar(b + '0');
+		}
+		++b;
+	}
+	++a;
+	b = 0;
+}
+putchar(10);
+return (0);
+}
