18a8568e569b8d6d5f0e2e6f4abedfa53ff1aa00
Brian Kimathi Muriuki <briankimathi>
2022-07-02 01:13:14 +0000

    Random

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	}
+		}
 	else if (last_digit  == 0)
 	{
 		printf("Last digit of %d is %d and is 0", n, last_digit);
