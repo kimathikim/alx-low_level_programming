ae0a4420256a83ceca622abbf1806a7553a4c8d6
Brian Kimathi Muriuki <briankimathi>
2022-07-05 23:17:23 +0000

    10 times (alpah)

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,15 @@
 void print_alphabet_x10(void)
 {
 	int i = 'a';
-	int j = 10;
+	int j = 0;
 
-	while (j >= 10)
+	while (j < 10)
 	{
 		while (i <= 'z')
 		{
 			_putchar(i);
 			++i;
 		}
-		--i;
-		_putchar(10);
-		i  = 'a';
+		i++;
 	}
 }
