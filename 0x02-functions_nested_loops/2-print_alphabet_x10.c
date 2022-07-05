9f9039d6547a78375da97698dc93732d344306ce
Brian Kimathi Muriuki <briankimathi>
2022-07-05 20:16:51 +0000

    Printing a to z 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print a to z 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+	int j = 0;
+
+	while (j < 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		J++;
+		_putchar(10);
+		a = 'a';
+	}
+}
