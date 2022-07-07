441924ee36b5f61047d3c4aab35c0afd3ba66950
Brian Kimathi Muriuki <briankimathi>
2022-07-07 19:23:09 +0000

    Print 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - funcltion
+ *
+ * Return: return value
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar("%d\n", i);
+		i++;
+	}
+	_putchar(10);
+}
