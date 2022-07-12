3c5450dd7f2180d61be065fb888244eb9a0074e3
Brian Kimathi Muriuki <briankimathi>
2022-07-12 16:20:06 +0000

    half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,38 +15,4 @@ void puts_half(char *str)
 	while (str[sublength] != '\0')
 		_putchar(str[sublength++]);
 	_putchar(10);
-}#include "main.h"
-/**
- * puts_half - function
- *
- * @str: pointer of parameter
- */
-void puts_half(char *str)
-{
-	int length = 0;
-	int sublength;
-
-	while (str[length] != '\0')
-		length++;
-	sublength = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
-	while (str[sublength] != '\0')
-		_putchar(str[sublength++]);
-	_putchar(10);
-}#include "main.h"
-/**
- * puts_half - function
- *
- * @str: pointer of parameter
- */
-void puts_half(char *str)
-{
-	int length = 0;
-	int sublength;
-
-	while (str[length] != '\0')
-		length++;
-	sublength = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
-	while (str[sublength] != '\0')
-		_putchar(str[sublength++]);
-	_putchar(10);
 }
