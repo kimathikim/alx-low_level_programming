621451c6ac357f85668b3c633b0dbe86c7d0536f
Brian Kimathi Muriuki <briankimathi>
2022-07-20 22:15:17 +0000

    finding a factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,3 +1,19 @@
 #include "main.h"
 /**
- * factorial
+ * factorial - find factorial of numbers
+ *
+ * @n: parameter
+ *
+ * Return: End program
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
+	else
+		return (-1);
+	return (0);
+}
