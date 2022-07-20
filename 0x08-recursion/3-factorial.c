115633bcbe994fea268b76b94404346ae56ad4eb
Brian Kimathi Muriuki <briankimathi>
2022-07-20 22:21:50 +0000

    Factorial of a num

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,18 @@
  */
 int factorial(int n)
 {
-	if (n > 0)
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
 	{
 		n = n * factorial(n - 1);
 		return (n);
 	}
-	else
-		return (-1);
 	return (0);
 }
