fb48c41094fcd43fa5c32f489335f550bc143f0d
Brian Kimathi Muriuki <briankimathi>
2022-07-20 21:48:30 +0000

    Length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function to count the
+ * number of caracters in the string
+ *
+ *  @s: parameter
+ *
+ *  Return: end function
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 1;
+
+	if (*s != 0)
+	{
+		s++;
+		n = n + strlen_recursion(s);
+
+		return (n);
+	}
+	return (0);
+}
