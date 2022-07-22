b3a6f8b278ddbf7023a171f4e203a8c53fd0b64d
Brian Kimathi Muriuki <briankimathi>
2022-07-22 23:28:53 +0000

    print nprog name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * @argc: arg count
+ * @argv: vector of strings with arguments
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(int argc, char* argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc -1);;
+	return (0);
+}
