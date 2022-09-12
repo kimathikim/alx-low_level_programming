a5ee6c85cadf24a8b599873573070199b2e975ae
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-12 19:02:52 +0300

    sum of arg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,20 +1,19 @@
 
 #include "variadic_functions.h"
 /**
- * sum_them_all - func to find sum
- *
+ * sum_them_all - func
  * @n: param
- * @param ...
- * @return int
+ * Return: int
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum, arg;
 
-	i = n;
-	sum = 0;
 	va_list(num);
 	va_start(num, n);
+
+	i = n;
+	sum = 0;
 	if (n == 0)
 		return (0);
 	arg = va_arg(num, int);
