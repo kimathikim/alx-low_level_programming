9a2427b72f8d4ef674c9dc6dfaaf82a8563bf810
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-12 18:50:41 +0300

    add separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,26 +1,28 @@
 #include "variadic_functions.h"
+/**
+ * print_numbers - func
+ * @separator: parameter
+ * @n: parameter
+ * @...
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i, arg;
-    va_list(num);
+	unsigned int i, arg;
 
-    i = n;
-    va_start(num,n);
-   
+	va_list(num);
+	i = n;
+	va_start(num, n);
 
-    while (i > 0)
-    {
-        printf("%d" ,  va_arg(num, int));
-        if (i != 1)
-        {
-            printf("%s", separator);
-        }
-        i--;
-    }
-}
-
-int main(void)
-{
-    print_numbers(", ", 4, 0, 98, -1024, 402);
-    return (0);
+	while (i > 0)
+	{
+		printf("%d", va_arg(num, int));
+		if (i != 1)
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+		i--;
+	}
 }
