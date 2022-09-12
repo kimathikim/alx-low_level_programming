6fb40dc5d2e11e7db58729804cde12e14ce034e7
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-12 13:18:48 +0300

    sum of arg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+
+#include "variadic_functions.h"
+/**
+ * sum_them_all - func to find sum
+ *
+ * @n: param
+ * @param ...
+ * @return int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum, arg;
+
+	i = n;
+	sum = 0;
+	va_list(num);
+	va_start(num, n);
+	arg = va_arg(num, int);
+	while (i > 0)
+	{
+		sum += arg;
+		arg = va_arg(num, int);
+		i--;
+	}
+	return (sum);
+	va_end(num);
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
+
+#endif
