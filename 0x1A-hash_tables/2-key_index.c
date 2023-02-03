c9d64eebdc672138d311365262b6bc55b6f6bac5
kimathikim <briankimathi94@gmail.com>
2023-02-04 00:10:06 +0300

    using the hash function to return Index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * key_index - function that finds the index where the index is
+ *
+ * @key: used to
+ * @size:size of the array of hash table
+ * Return: the index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	index = index % size;
+
+	return (index);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef __MAIN__
 #define __MAIN__
 
+/*Librariea*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,10 +31,12 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
- #endif
+#endif
