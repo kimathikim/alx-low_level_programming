82f6fcb973a82a1c0baa6abb25b484bbea4a4356
Brian Kimathi M <briankimathi94@gmail.com>
2023-12-11 18:50:06 +0300

    linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t size, int value);
+
+#endif
