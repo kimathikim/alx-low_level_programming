93961b6c9bd3c3207349c307fa3e83c9dbe5542e
Brian Kimathi M <briankimathi94@gmail.com>
2023-12-13 10:12:41 +0300

    binary and linear  search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,30 @@
 #include "search_algos.h"
-
 /**
- * binary_search - function to search the index of a value
- * sorted array of numbers
- * @array: an array of sorted members
- * @size: the size of of the array
- * Value: the value to search
- *
- * return: return the the index of of the value being searched or -1
- * if the array is NULL or the VALUE being searched does not exist
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located
  */
-int binary_search(int *array, size_t size, int value) {
-  size_t i, head, tail, mid;
+int binary_search(int *array, size_t size, int value)
+{
+	size_t head = 0, tail = size - 1, i = 0;
 
-  head = 0;
-  tail = size - 1;
-  if (array == NULL)
-    return -1;
-  while (head <= tail) {
-    printf("Searching in the array: ");
-    for (i = head; i <= tail; i++)
-      if (i < tail)
-        printf("%lu, ", i);
-      else
-        printf("%lu ", i);
-
-    printf("\n");
-    mid = (head + tail) / 2;
-    if (array[mid] == value)
-      return mid;
-    if (array[mid] < value)
-      head = mid + 1;
-    else
-      tail = mid - 1;
-  }
-  return -1;
+	if (array == NULL)
+		return (-1);
+	while (head <= tail){
+		printf("Searching in array: ");
+		for (i = head; i < tail; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		i = (head + tail) / 2;
+		if (array[i] == value)
+			return (i);
+		else if (array[i] > value)
+			tail = i - 1;
+		else
+			head = i + 1;
+	}
+	return (-1);
 }
